8f1a29d2946f4258d3e7a2b82b77a21994246b11
Diogo Martins <diogopro1400@gmail.com>
2026-02-05 16:04:29 +0100

    chore(pascals-triangle): exo fini [30] [DONE]

--- exercices/pascals-triangle/pascals-triangle.js
+++ exercices/pascals-triangle/pascals-triangle.js
@@ -3,6 +3,16 @@
 // convenience to get you started writing code faster.
 //
 
-export const rows = () => {
-  throw new Error('Remove this line and implement the function');
+export const rows = (n) => {
+	const result = [];
+
+	for (let i = 0; i < n; i++) {
+		const row = Array(i + 1).fill(1);
+
+		for (let j = 1; j < i; j++) {
+			row[j] = result[i - 1][j - 1] + result[i - 1][j];
+		}
+		result.push(row);
+	}
+	return result;
 };
